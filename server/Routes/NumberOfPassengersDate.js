076a306b25e0dfbaa34dd749f39be7ea1c408c54
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-01-12 23:23:56 +0530

    Works Fine

--- server/Routes/NumberOfPassengersDate.js
+++ server/Routes/NumberOfPassengersDate.js
@@ -10,7 +10,7 @@ const db=mysql.createConnection({
 });
 
 router.get('/', (req, res) => {    
-    db.query('select count(*) as Count from passenger p left join booking b on p.passenger_ID=b.passenger_ID left join flight_schedule fs on b.schedule_ID=fs.schedule_ID left join flight f on fs.flight_ID=f.flight_ID where f.destination="CGK" and fs.starting_date between "'+req.query.startDate1+'" and "'+req.query.endDate1+'"', (error, results) => {
+    db.query('select count(*) as Count from passenger p left join booking b on p.passenger_ID=b.passenger_ID left join flight_schedule fs on b.schedule_ID=fs.schedule_ID left join flight f on fs.flight_ID=f.flight_ID where f.destination="'+req.query.dest1+'" and fs.starting_date between "'+req.query.startDate1+'" and "'+req.query.endDate1+'"', (error, results) => {
       if (error) throw error;
       res.send(JSON.stringify(results));
     });
